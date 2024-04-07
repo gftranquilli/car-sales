e3cfaa4227f6cddf32a8f2b8912fd51b13595473
Gabriel Farias Tranquilli <gabrielftranquilli@gmail.com>
2024-04-07 16:50:18 -0300

    Criados Selectbox com tipos de gráfico e Menu Dropdown com os gráficos em si.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Projeto TripleTen para criar um web app com informações úteis sobre vendas de carros
 
 No primeiro commit foram realizados testes apenas para garantir que a base do web app estava funcionando.
+
+No segundo commit foram criadas duas checkbox para decidir qual tipo de gráfico criar. Ao clicar na checkbox, surge um menu dropdown daquele tipo, pedindo para escolher qual o gráfico daquele tipo a ser criado. Por fim o botão 'Gerar gráfico' deve ser clicado para confirmar as seleções. O modelo atual permite a seleção de ambos os tipos de gráfico, porém cria apenas 1 de cada.
--- app.py
+++ app.py
@@ -7,14 +7,74 @@ car_data = pd.read_csv('vehicles.csv')
 
 st.header('Análise de dados de veículos')  # título
 
-hist_button = st.button('Criar histograma')  # criar um botão
+# criar um checkbox para histograma
+hist_checkbox = st.checkbox('Criar um histograma')
+# criar um checkbox para gráfico de dispersão
+scatter_checkbox = st.checkbox('Criar gráfico de dispersão')
+start_button = st.button('Gerar gráfico')  # criar um botão para gerar gráfico
 
-if hist_button:  # se o botão for clicado
-    # escrever uma mensagem
-    st.write(
-        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+if hist_checkbox:
+    hist_selectbox = st.selectbox('Escolha o histograma a ser criado:', ('Odômetro',
+                                                                         'Tipo de veículo', 'Condição do veículo', 'Dias de listado'))
+if scatter_checkbox:
+    scatter_selectbox = st.selectbox(
+        'Escolha o gráfico de dispersão a ser criado:', ('Odômetro x Preço', 'Ano x Preço'))
 
-    # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
+if start_button:  # se o botão for pressionado
+    if hist_checkbox:  # se histograma for selecionado
+        if hist_selectbox == 'Odômetro':
+            # escrever uma mensagem
+            st.write(
+                'Criando um histograma para a milhagem exibida no odômetro dos veículos...')
+            # criar um histograma
+            fig = px.histogram(car_data, x='odometer', labels={
+                'odometer': 'Odômetro em Milhas'})
+            fig.show()  # exibindo
+        # escrever uma mensagem
+        if hist_selectbox == 'Tipo de veículo':
+            st.write(
+                'Criando um histograma para o tipo de veículo dos veículos...')
+            # criar um histograma
+            fig = px.histogram(car_data, x='type', labels={
+                'type': 'Tipo de Veículo'})
+            fig.show()  # exibindo
+        if hist_selectbox == 'Condição do veículo':
+            # escrever uma mensagem
+            st.write(
+                'Criando um histograma para a condição dos veículos...')
+            # criar um histograma
+            fig = px.histogram(car_data, x='condition', labels={
+                'condition': 'Condição do Veículo'})
+            fig.show()  # exibindo
+        if hist_selectbox == 'Dias de listado':
+            # escrever uma mensagem
+            st.write(
+                'Criando um histograma para o tempo (em dias) desde que os veículos foram listados...')
+            # criar um histograma
+            fig = px.histogram(car_data, x='days_listed', labels={
+                'days_listed': 'Dias desde Listado'})
+            fig.show()  # exibindo
+
+        # exibir um gráfico Plotly interativo
+        st.plotly_chart(fig, use_container_width=True)
+
+    if scatter_checkbox:  # se gráfico de dispersão for selecionado
+        if scatter_selectbox == 'Odômetro x Preço':
+            # escrever uma mensagem
+            st.write(
+                'Criando um gráfico de dispersão para a milhagem exibida no odômetro e o preço dos veículos...')
+            # criar um gráfico de dispersão
+            fig2 = px.scatter(car_data, x='odometer', y='price', labels={
+                'odometer': 'Odômetro em Milhas', 'price': 'Preço em Dólares'})
+            fig2.show()  # exibindo
+        if scatter_selectbox == 'Ano x Preço':
+            # escrever uma mensagem
+            st.write(
+                'Criando um gráfico de dispersão para o ano e o preço dos veículos...')
+            # criar um gráfico de dispersão
+            fig2 = px.scatter(car_data, x='model_year', y='price', labels={
+                'year': 'Ano de fabricação', 'price': 'Preço em Dólares'})
+            fig2.show()
+
+        # exibir um gráfico Plotly interativo
+        st.plotly_chart(fig2, use_container_width=True)
